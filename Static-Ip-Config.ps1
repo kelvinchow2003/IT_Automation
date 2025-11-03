fdfc80143da162f17eb3afc851cefff13fe085b3
Kelvin Chow <kelvin1.chow@torontomu.ca>
2025-11-03 11:49:36 -0500

    Update and rename Ethernet-Config-sketchy.ps1 to Static-Ip-Config.ps1

---
+++ Static-Ip-Config.ps1
@@ -0,0 +1,17 @@
+﻿# Define adapter name
+$adapterName = "Ethernet"
+
+# Disable IPv6
+Disable-NetAdapterBinding -Name $adapterName -ComponentID ms_tcpip6
+
+# Set static IP address
+New-NetIPAddress -InterfaceAlias $adapterName -IPAddress "123.123.123.123" -PrefixLength 24 -DefaultGateway "123.123.123.123"
+
+# Set DNS servers
+Set-DnsClientServerAddress -InterfaceAlias $adapterName -ServerAddresses ("123.123.123.123", "123.123.123.123")
+
+
+
+# Confirm changes
+Get-NetIPAddress -InterfaceAlias $adapterName
+Get-DnsClientServerAddress -InterfaceAlias $adapterName
